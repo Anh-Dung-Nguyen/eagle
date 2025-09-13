ef6cb058b57dc2e03087e56753071a36e4faea80
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-09-13 15:54:34 +0200

    Add footer

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../Common/Header';
+import Footer from '../Common/Footer';
 
 const UserLayout = () => {
   return (
     <>
       {/* Header */}
       <Header />
+
+      {/* Main content */}
+
+      {/* Footer */}
+      <Footer />
     </>
   )
 }
