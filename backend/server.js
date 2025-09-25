d59003592b58a08ec497cbbe21c4e8b3437796b5
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-09-25 12:14:00 +0200

    Add API route for user

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -17,6 +18,10 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("Welcome to Eagle API");
 });
+
+// API Routes
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
