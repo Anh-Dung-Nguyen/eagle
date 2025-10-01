10eb294011881d6f2558df6b94573486b1ff7598
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-10-01 18:59:38 +0200

    Add checkout model

---
+++ backend/models/Checkout.js
@@ -0,0 +1,108 @@
+const mongoose = require("mongoose");
+
+const checkoutItemSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        image: {
+            type: String,
+            required: true,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+
+        size: String,
+        color: String,
+
+        quantity: {
+            type: Number,
+            required: true,
+        },
+    },
+
+    {_id: false},
+);
+
+const checkoutSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        checkoutItems: {
+            type: [checkoutItemSchema],
+            required: true,
+        },
+
+        shippingAddress: {
+            address: {
+                type: String,
+                required: true,
+            },
+
+            city: {
+                type: String,
+                required: true,
+            },
+
+            postalCode: {
+                type: String,
+                required: true,
+            },
+
+            country: {
+                type: String,
+                required: true,
+            },
+        },
+
+        paymentMethod: {
+            type: String,
+            required: true,
+        },
+
+        totalPrice: {
+            type: Number,
+            required: true,
+        },
+
+        isPaid: {
+            type: Boolean,
+            default: false,
+        },
+
+        paidAt: Date,
+
+        paymentStatus: {
+            type: String,
+            default: "pending",
+        },
+
+        paymentDetails: mongoose.Schema.Types.Mixed,
+        
+        isFinalized: {
+            type: Boolean,
+            default: false,
+        },
+
+        finalizedAt: Date,
+    },
+
+    {timestamps: true},
+);
+
+module.exports = mongoose.model("Checkout", checkoutSchema);
