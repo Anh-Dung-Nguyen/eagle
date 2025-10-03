bbc42faa0615c4cc9fa2373e7c8878756eaa4213
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-10-03 15:39:51 +0200

    Add routes for subscribe and admin

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
+const subscribeRoutes = require("./routes/subscribeRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 const app = express();
 
 app.use(express.json());
@@ -31,6 +33,10 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+app.use("/api", subscribeRoutes);
+
+// Admin
+app.use("/api/admin/users", adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
