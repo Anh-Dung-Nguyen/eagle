45074e90a0a6ef8d785d6cec027e92ba455d5634
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-10-09 21:23:43 +0200

    Merge cart when not log in

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import login from "../assets/login.webp";
 import {loginUser} from "../redux/slices/authSlice";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { mergeCart } from '../redux/slices/cartSlice';
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const {user, guestId} = useSelector((state) => state.auth);
+    const {cart} = useSelector((state) => state.cart);
+
+    // Get redirect parameter and check if it is checkout or something
+    const redirect = new URLSearchParams(location.search).get("redirect") || "/";
+    const isCheckoutRedirect = redirect.includes("checkout");
+
+    useEffect(() => {
+        if (user) {
+            if (cart?.products.length > 0 && guestId) {
+                dispatch(mergeCart({guestId, user})).then(() => {
+                    navigate(isCheckoutRedirect ? "/checkout" : "/");
+                });
+            } else {
+                navigate(isCheckoutRedirect ? "/checkout" : "/");
+            }
+        }
+    }, [user, guestId, cart, navigate, isCheckoutRedirect, dispatch]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -66,7 +87,7 @@ const Login = () => {
                     <p className = 'mt-6 text-center text-sm'>
                         Don't have account?{" "}
                         <Link
-                            to = "/register"
+                            to = {`/register?redirect=${encodeURIComponent(redirect)}`}
                             className = 'text-blue-500'
                         >
                             Register
