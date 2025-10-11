28353eb30c1d8c4519fc49b7a0b7123b881d0352
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-10-11 09:53:06 +0200

    Add API to page

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -1,43 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
+import { fetchOrderDetails } from '../redux/slices/orderSlice';
 
 const OrderDetailsPage = () => {
     const {id} = useParams();
-
-    const [orderDetails, setOrderDetails] = useState(null);
+    const dispatch = useDispatch();
+    const {orderDetails, loading, error} = useSelector((state) => state.orders);
 
     useEffect(() => {
-        const mockOrderDetails = {
-            _id: id,
-            createdAt: new Date(),
-            isPaid: true,
-            isDelivered: false,
-            paymentMethod: "PayPal",
-            shippingMethod: "Standard",
-            shippingAddress: {
-                city: "Hanoi",
-                country: "Vietnam",
-            },
-            orderItems: [
-                {
-                    productId: "1",
-                    name: "Jacket",
-                    price: 120,
-                    quantity: 1,
-                    image: "https://picsum.photos/500/500?random=1"
-                },
-                {
-                    productId: "2",
-                    name: "Shirt",
-                    price: 150,
-                    quantity: 2,
-                    image: "https://picsum.photos/500/500?random=2"
-                },
-            ],
-        };
+        dispatch(fetchOrderDetails(id));
+    }, [dispatch, id]);
 
-        setOrderDetails(mockOrderDetails);
-    }, [id]);
+    if (loading) {
+        return <p>
+            Loading ...
+        </p>
+    }
+
+    if (error) {
+        return <p>
+            Error: {error}
+        </p>
+    }
 
     return (
         <div className = 'max-w-7xl mx-auto p-4 sm:p-6'>
